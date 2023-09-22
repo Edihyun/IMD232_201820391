dfc168c270f3d68bb1be9ebf99e12bc63a61e35c
Edihyun <sei9879@nanver.com>
2023-09-22 21:02:49 +0900

    프로제긑

---
+++ src/w3/sketches/sketch2/script/sketch.js
@@ -0,0 +1,36 @@
+let pos;
+let vel;
+let acc;
+let radius = 50;
+
+function setup() {
+  setCanvasContainer('mySketchGoesHere', 3, 2, true);
+  background('white');
+  pos = createVector(width / 2, height / 2);
+  vel = createVector(0, 0);
+  acc = createVector();
+  console.log(pos);
+  console.log(vel);
+  ellipse(pos.x, pos.y, 50);
+}
+function draw() {
+  background(255);
+  // pos.add(vel);
+  acc = p5.vector.random2D();
+  // if (pos.x < 0) {
+  //   vel.x *= -1;
+  // } else if (pos.x > width) {
+  //   vel.x *= -1;
+  // }
+  pos.add(vel);
+  acc.mult(2);
+  vel.add(acc);
+  if (pos.x - radius < 0 || pos.x + radius > width) {
+    vel.x *= -1;
+  }
+  if (pos.y - radius < 0 || pos.y + radius > height) {
+    vel.y *= -1;
+  }
+
+  ellipse(pos.x, pos.y, 2 * radius);
+}
